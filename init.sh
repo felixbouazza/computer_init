2b9b98d6f989cf87f94591991765943b6b2d6302
Félix <fbouazza.pro@gmail.com>
2021-11-13 14:38:39 +0100

    init file

---
+++ init.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# ## Git
+
+# sudo apt update
+# sudo apt install git
+
+# ## Terminator
+
+# sudo apt install terminator
+
+# ## Keepass2
+
+# sudo apt install keepass2
+
+# ## NodeJS v16.0
+# wget -qO- https://deb.nodesource.com/setup_16.x | sudo -E bash -
+# sudo apt install -y nodejs
+
+# ## VSCODE
+
+# wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+# sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+# sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+# rm -f packages.microsoft.gpg
+
+# sudo apt install apt-transport-https
+# sudo apt update
+# sudo apt install code
+
+## Docker
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+## For docker installation, you have to logout and login and run "newgrp docker"
